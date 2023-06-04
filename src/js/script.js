2200b3129663435b7a3156e1e43db568138d44a5
NAKAGAWA-neko <goginzahiro@gmail.com>
2023-06-05 01:54:30 +0900

    修正しました

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#myElement").bgSwitcher({
+    images: ["image1.jpg", "image2.jpg", "image3.jpg"],
+    interval: 5000,
+    effect: "fade",
+    duration: 2000,
+  });
+});
